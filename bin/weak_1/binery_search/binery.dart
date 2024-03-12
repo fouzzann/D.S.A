3b48cee4d31d63dbaf705a89c8ce51b2542824f4
Fouzan.p <147602953+fouzzann@users.noreply.github.com>
2024-03-12 17:18:47 +0530

    added

--- bin/weak_1/binery_search/binery.dart
+++ bin/weak_1/binery_search/binery.dart
@@ -31,31 +31,3 @@ main() {
 }
 
 
-
-//CHARACTERS
-// class Sample{
-//   void display(List<dynamic>nums , dynamic left , dynamic right , dynamic target){
-//     nums.sort(); 
-//     print('$nums');
-//     while(left<=right){
-//       var middle = ( left+ right)~/2;
-//       if(nums[middle]== target){
-//         print('$middle');
-//         break;
-//       }else if(nums [middle].compareTo(target)>0){
-//         right = middle --;
-//       }else{
-//        left = middle ++;
-//       }
-//     }
-//   }
-// }
-//  main() {
-//  Sample sample = Sample();
-//  List<dynamic> nums = ["apple","ple","ap","a"];
-//  dynamic target = "ap";
-//  int left = 0;
-//  int right = nums.length;
-//  sample.display(nums, left, right, target);
-
-// }
